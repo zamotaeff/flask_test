7a89e8a7ed41d14f2b843745f46596feac1eb340
levi.german <levi.german@yandex.ru>
2022-05-13 21:49:07 +0300

    Add docstrings in flask route functions

--- app.py
+++ app.py
@@ -8,21 +8,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def page_index():
-    """"""
+    """Представление для списка кандидатов"""
 
     return get_candidates_list()
 
 
 @app.route("/candidates/<int:uid>/")
 def page_candidates(uid):
-    """"""
+    """Представление для вывода информации о кандидате по его ID"""
 
     return get_candidate_info(uid)
 
 
 @app.route('/skills/<skill>/')
 def page_skills(skill):
-    """"""
+    """Представление для списка кандидатов, имеющих опеределенный навык"""
 
     return get_candidates_with_skill(skill)
 
