52211b496411c7433c68d3c37cd11516709194c1
levi.german <levi.german@yandex.ru>
2022-05-18 21:34:24 +0300

    Исправил функцию get_candidates_with_skill(skill) Переимиеновал в get_candidates_by_skill(skill_name) и стал использовать конкатенацию строк

--- project/utils.py
+++ project/utils.py
@@ -12,18 +12,18 @@ def get_candidates():
     """Функция форматирует данные
     и оттадет форматированную строку"""
 
-    pre_format_list = '<pre>'
+    pre_format_string = '<pre>'
 
     candidates_data = load_candidates_from_json()
 
     for item in candidates_data:
         format_string = f'Имя - {item["name"]}\nПозиция - {item["position"]}\nНавыки - {item["skills"]}\n\n'
 
-        pre_format_list += format_string
+        pre_format_string += format_string
 
-    pre_format_list += '</pre>'
+    pre_format_string += '</pre>'
 
-    return ''.join(pre_format_list)
+    return pre_format_string
 
 
 def get_candidate(candidate_id):
@@ -38,6 +38,25 @@ def get_candidate(candidate_id):
                    f'<pre>Имя - {item["name"]}\n' \
                    f'Позиция - {item["position"]}\n' \
                    f'Навыки - {item["skills"]}</pre>'
+
+
+def get_candidates_by_skill(skill_name):
+    """Функция находит всех кандидатов с полем skills
+    и возвращает форматированную строку"""
+
+    candidates_data = load_candidates_from_json()
+
+    pre_format_string = '<pre>'
+
+    for item in candidates_data:
+        if skill_name.lower() in item['skills'] or skill_name.capitalize() in item['skills']:
+            format_string = f'Имя - {item["name"]}\nПозиция - {item["position"]}\nНавыки - {item["skills"]}\n\n'
+
+            pre_format_string += format_string
+
+    pre_format_string += '</pre>'
+
+    return pre_format_string
 
 
 def get_candidates_with_skill(skill):
